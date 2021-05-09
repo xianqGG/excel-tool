fc88f8418f2e0eba125b06934ee7c5c7799bfec3
daief <1437931235@qq.com>
2021-05-09 21:08:40 +0800

    wip: refine

--- src/__test__/format.test.js
+++ src/__test__/format.test.js
@@ -1,8 +1,8 @@
-const { format } = require("../format");
+const { format, numToChinese, formatWithPostHandle } = require("../format");
 
 describe("format", () => {
   it("递增 & 单双数", () => {
-    expect(format("永丰村：1组1号-2号，2组1号-2号")).toEqual([
+    expect(format("永丰村：1组1号至2号，2组1号-2号")).toEqual([
       ["永丰村", "1组1号"],
       ["永丰村", "1组2号"],
       ["永丰村", "2组1号"],
@@ -49,6 +49,58 @@ describe("format", () => {
 
   it("count", () => {
     expect(format("太平社区：1-1（4个）、1-2（3个）")).toEqual([
+      ["太平社区", "1-1"],
+      ["太平社区", "1-1"],
+      ["太平社区", "1-1"],
+      ["太平社区", "1-1"],
+      ["太平社区", "1-2"],
+      ["太平社区", "1-2"],
+      ["太平社区", "1-2"],
+    ]);
+  });
+});
+
+describe("numToChinese", () => {
+  it("numToChinese", () => {
+    expect(numToChinese(0)).toBe("零");
+    expect(numToChinese(1)).toBe("一");
+
+    expect(numToChinese(10)).toBe("十");
+    expect(numToChinese(11)).toBe("十一");
+
+    expect(numToChinese(20)).toBe("二十");
+    expect(numToChinese(21)).toBe("二十一");
+
+    expect(numToChinese(99)).toBe("九十九");
+    expect(numToChinese(100)).toBe("一百");
+    expect(numToChinese(101)).toBe("一百零一");
+  });
+});
+
+describe("formatWithPostHandle", () => {
+  it("递增 & 单双数", () => {
+    expect(formatWithPostHandle("永丰村：1组1号至2号，2组1号-2号")).toEqual([
+      ["永丰村一组", "1"],
+      ["永丰村一组", "2"],
+      ["永丰村二组", "1"],
+      ["永丰村二组", "2"],
+    ]);
+  });
+
+  it("normal", () => {
+    expect(formatWithPostHandle("复兴路：2-1、5")).toEqual([
+      ["复兴路", "2-1"],
+      ["复兴路", "5"],
+    ]);
+
+    expect(formatWithPostHandle("复兴村：19号、25号")).toEqual([
+      ["复兴村", "19"],
+      ["复兴村", "25"],
+    ]);
+  });
+
+  it("count", () => {
+    expect(formatWithPostHandle("太平社区：1-1（4个）、1-2（3个）")).toEqual([
       ["太平社区", "1-1"],
       ["太平社区", "1-1"],
       ["太平社区", "1-1"],
--- src/format.js
+++ src/format.js
@@ -11,6 +11,7 @@ function isNun(v) {
 
 function format(input) {
   if (typeof input !== "string" || !input) return [];
+  input = input.trim();
   const [field, value] = input.split("：");
 
   if (!value) {
@@ -92,11 +93,72 @@ function parseValue(val = "") {
   return res;
 }
 
+function numToChinese(num) {
+  var chnNumChar = ["零", "一", "二", "三", "四", "五", "六", "七", "八", "九"];
+  var chnUnitSection = ["", "万", "亿", "万亿", "亿亿"];
+  var chnUnitChar = ["", "十", "百", "千"];
+
+  function SectionToChinese(section) {
+    var strIns = "",
+      chnStr = "";
+    var unitPos = 0;
+    var zero = true;
+    while (section > 0) {
+      var v = section % 10;
+      if (v === 0) {
+        if (!zero) {
+          zero = true;
+          chnStr = chnNumChar[v] + chnStr;
+        }
+      } else {
+        zero = false;
+        strIns = chnNumChar[v];
+        strIns += chnUnitChar[unitPos];
+        chnStr = strIns + chnStr;
+      }
+      unitPos++;
+      section = Math.floor(section / 10);
+    }
+    return chnStr;
+  }
+
+  var unitPos = 0;
+  var strIns = "",
+    chnStr = "";
+  var needZero = false;
+
+  if (num === 0) {
+    return chnNumChar[0];
+  }
+
+  while (num > 0) {
+    var section = num % 10000;
+    if (needZero) {
+      chnStr = chnNumChar[0] + chnStr;
+    }
+    strIns = SectionToChinese(section);
+    strIns += section !== 0 ? chnUnitSection[unitPos] : chnUnitSection[0];
+    chnStr = strIns + chnStr;
+    needZero = section < 1000 && section > 0;
+    num = Math.floor(num / 10000);
+    unitPos++;
+  }
+
+  chnStr = chnStr.replace(/^一十/, "十");
+
+  return chnStr;
+}
+
 function postHandle(field, value) {
   const regex = /(\d+)号/;
   if (regex.test(value)) {
-    return [field + value.replace(regex, ""), regex.exec(value)[1]];
+    [field, value] = [field + value.replace(regex, ""), regex.exec(value)[1]];
   }
+
+  field = field.replace(/(\d+)组/, (match, n) =>
+    match.replace(n, numToChinese(+n))
+  );
+
   return [field, value];
 }
 
@@ -107,3 +169,4 @@ function formatWithPostHandle(input) {
 module.exports.formatWithPostHandle = formatWithPostHandle;
 module.exports.format = format;
 module.exports.parseValue = parseValue;
+module.exports.numToChinese = numToChinese;
