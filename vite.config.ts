5956208931ea8e370b1a752fb4c5567e09e8f7a0
daief <1437931235@qq.com>
2021-07-02 23:11:03 +0800

    wip: build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
-})
+  base: "./",
+  plugins: [reactRefresh()],
+  build: {
+    outDir: "docs",
+  },
+});
