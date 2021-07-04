347b66a5ce9e74942312c2e82b9035a8f191baa3
daief <1437931235@qq.com>
2021-07-04 12:00:10 +0800

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
+import analyzer from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [reactRefresh()],
+  plugins: [reactRefresh(), process.env.ANALYZER && analyzer()].filter(Boolean),
   build: {
     outDir: "docs",
   },
